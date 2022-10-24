9dc007af9c9320937410e3daf1f92073370b3eef
Andre Luis Pleti <andre.pleti@hotmail.com.br>
2022-10-24 17:10:56 -0300

    Primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "principal", loadChildren: () => import("./views/principal/principal.module").then(m => m.PrincipalModule) },
+  { path: "secundario", loadChildren: () => import("./views/secundario/secundario.module").then(m => m.SecundarioModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: []
 })
 export class AppComponent {
-  title = 'ExemploFront';
+
+
+
 }
